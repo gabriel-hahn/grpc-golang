632ea7181c65932d10cb84fff718c51c05096a13
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2023-08-09 20:30:38 -0300

    feat: add ReadBlog service

---
+++ blog/server/read.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+
+	pb "github.com/gabriel-hahn/grpc-golang/blog/proto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) ReadBlog(ctx context.Context, in *pb.BlogId) (*pb.Blog, error) {
+	oid, err := primitive.ObjectIDFromHex(in.Id)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Cannot parse ID")
+	}
+
+	data := &BlogItem{}
+	filter := bson.M{"_id": oid}
+	res := collection.FindOne(ctx, filter)
+
+	if err = res.Decode(data); err != nil {
+		return nil, status.Errorf(codes.NotFound, "Cannot find blog with the ID provided")
+	}
+
+	return documentToBlog(data), nil
+}
